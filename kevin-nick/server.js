47ec2cbeb059288a9b56998c483348468b8af980
Kevinoh47 <kohok47@gmail.com>
2018-08-18 16:12:26 -0700

    finished changes to server.js

--- kevin-nick/server.js
+++ kevin-nick/server.js
@@ -18,12 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-// REVIEW: These are routes for requesting HTML resources.
+// REVIEWed: These are routes for requesting HTML resources.
 app.get('/new-article', (request, response) => {
   response.sendFile('new.html', { root: './public' });
 });
 
-// REVIEW: These are routes for making API calls to enact CRUD operations on our database.
+// REVIEWed: These are routes for making API calls to enact CRUD operations on our database.
 app.get('/articles', (request, response) => {
   client.query(`
     SELECT a.article_id, a.title, a.category, a.published_on, a.body, 
@@ -47,40 +47,40 @@ app.post('/articles', (request, response) => {
     function(err) {
       if (err) {
         console.error(err);
-        // REVIEW: Early return here prevents queryTwo from running if there's an error
+        // REVIEWed: Early return here prevents queryTwo from running if there's an error
         return response.status(500).send(err);
       }
 
-      // REVIEW: This is our second query, to be executed when this first query is complete.
+      // REVIEWed: This is our second query, to be executed when this first query is complete.
       queryTwo();
     }
   )
 
   function queryTwo() {
-    let SQL = 'SELECT author_id FROM authors WHERE author = $1;';
+    let SQL = `SELECT author_id FROM authors WHERE author = $1;`;
     let values = [request.body.author];
     client.query(SQL, values,
       function(err, result) {
         if (err) {
           console.error(err);
-          // REVIEW: Early return here prevents queryThree from running if there's an error
+          // REVIEWed: Early return here prevents queryThree from running if there's an error
           return response.status(500).send(err);
         }
 
-        // REVIEW: This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query.
+        // REVIEWed: This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query.
         queryThree(result.rows[0].author_id);
       }
     )
   }
 
   function queryThree(author_id) {
-    let SQL = 'INSERT INTO articles(title, category, published_on, body, author_id) VALUES($1, $2, $3, $4, $5);';
+    let SQL = `INSERT INTO articles(title, category, published_on, body, author_id) VALUES($1, $2, $3, $4, $5);`;
     let values = [request.body.title, request.body.category, request.body.published_on, request.body.body, author_id];
     client.query(SQL, values,
       function(err) {
         if (err) {
           console.error(err);
-          // REVIEW: Early return here prevents sending again if there's an error
+          // REVIEWed: Early return here prevents sending again if there's an error
           return response.status(500).send(err);
         }
 
@@ -92,7 +92,7 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', function(request, response) {
 
-  console.log('INSIDE UPDATE!! request object, params property:', request.params);
+  console.log('INSIDE UPDATE!! request object, params property:', request.params); // look here...
 
   let SQL = `UPDATE authors SET author=$1, author_url=$2 WHERE author_id=$3;`;
   let values = [request.body.author, request.body.author_url,request.body.author_id];
@@ -126,7 +126,7 @@ app.delete('/articles/:id', (request, response) => {
     });
 });
 
-//Trying to get a particular article, so that it can be updated.
+//Trying to get a particular article, so that it can be updated. This wasn't specified in the lesson. Presumably comes later?
 app.get('/articles/:id', (request, response) => {
   console.log('INSIDE NEW APP.GET, request.params: ',request.params);
   let SQL = `SELECT a.article_id, a.title, a.category, a.published_on, a.body, 
@@ -157,7 +157,7 @@ app.delete('/articles', (request, response) => {
     });
 });
 
-// REVIEW: This calls the loadDB() function, defined below.
+// REVIEWed: This calls the loadDB() function, defined below.
 loadDB();
 
 app.listen(PORT, () => {
@@ -172,7 +172,7 @@ app.listen(PORT, () => {
 function loadAuthors() {
   fs.readFile('./public/data/hackerIpsum.json', 'utf8', (err, fd) => {
     JSON.parse(fd).forEach(ele => {
-      let SQL = 'INSERT INTO authors(author, author_url) VALUES($1, $2) ON CONFLICT DO NOTHING';
+      let SQL = `INSERT INTO authors(author, author_url) VALUES($1, $2) ON CONFLICT DO NOTHING`;
       let values = [ele.author, ele.author_url];
       client.query(SQL, values);
     })
